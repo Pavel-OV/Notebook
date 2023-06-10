1a5a83d6d76facb3681deecc83027bd741c181b1
Pavel <black_yaguar@mail.ru>
2023-06-10 18:42:19 +0500

    Создание навигациноого меню

---
+++ navigation.py
@@ -0,0 +1,27 @@
+def main():
+    global message
+    # message = load_message()
+    while True:
+        choice = input("""Выбери действие, нажми
+        '1' - создание новой записи
+        '1' - просмотр  запись
+        '1' - изменить запись
+        '1' - удалить запись
+        '1' - просмотреть все записи
+        '1' - выход
+        """)
+        if choice == "1":
+            pass
+        if choice == "1":
+            pass
+        if choice == "1":
+            pass
+        if choice == "1":
+            pass
+        if choice == "1":
+            pass
+        if choice == "1":
+            pass
+        else:
+            print("Не правильно выбрана команда")
+
