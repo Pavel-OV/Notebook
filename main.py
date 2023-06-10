85c4442ba65baa379cc1a070bfa5d6b5e83b41fc
Pavel <black_yaguar@mail.ru>
2023-06-10 18:48:23 +0500

    Наброски для запуска программы

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import time
-from datetime import datetime
-
-
-timedate = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+from class_notes import *
+import navigation as nav
 
 
 # timedate=(timedate.day, timedate.month, timedate.year)
 # print(f"Сегодня {timedate.day}-{timedate.month}-{timedate.year}")
-print(timedate)
 
 
 
 print( '''Привет! Это твоя записная книжка. Давай расскажу что я умею.
 Мы можем создать новую запись, найти её, изменить или удалить,
 а также показать все записи.''')
+
+
+
+if __name__ == '__main__':
+#     notes = Notes()  # Контроллер - тут все происходит
+#     notes.run()
+    nav.navigation()
+    
