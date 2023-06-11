f32c3f61fe6d7342b3a62d7feb2835dd83f66cff
Pavel <black_yaguar@mail.ru>
2023-06-11 15:06:32 +0500

    Редактировние класса

--- class_notes.py
+++ class_notes.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-
+import os.path
+import json
 class Notes:
    
 
@@ -9,37 +10,25 @@ class Notes:
         self.note_body = note_body
         self.time_of_creation = time_of_creation
         self.last_modified_time = time_of_creation
-#datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-    def get_id(self):
-        return self.id
+        #datetime.now().strftime('%d-%m-%Y %H:%M:%S')
     
-    def get_heading(self):
-        return self.heading
+    def to_dict(self):
+    	return {
+        "id": self.id,
+        "heading": self.heading,
+        "note_body": self.note_body,
+        "time_of_creation":  str(self.time_of_creation),
+        "last_modified_time": str(self.last_modified_time)    }
+    @classmethod
+    def from_dict(cls,d):
+        return cls(
+            id= d["id"],
+            heading = d["heading"],
+            note_body = d["note_body"],
+            time_of_creation = datetime.fromisoformat(d["time_of_creation"]),
+                )
     
-    def get_note_body(self):
-        return self.note_body
-    
-    def get_time_of_creation(self):
-        return self.time_of_creation
-    
-    def set_id(self,n):
-         self.id = n
-    
-    def set_heading(self,n):
-        self.heading = n
-    
-    def set_note_body(self,n):
-        self.note_body = n
-    
-    def set_time_of_creation(self,n):
-       self.time_of_creation = n
-    
-    def __str__(self):
-        info = ''
-        info += 'f{self.id}'
-        info += 'f{self.neading}; '
-        info += 'f{note_body}; '
-        info += 'f{self.time_of_creation}'
-        return info
 
+    def __repr__(self):
+        return f"<Note {self.heading} ({self.id})>"
 
