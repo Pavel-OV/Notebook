ec2c50ff4c91ad3f72450c88926440054a6a0e96
Pavel <black_yaguar@mail.ru>
2023-06-11 21:34:16 +0500

    Добовление функции обновление записи

--- read_update_message.py
+++ read_update_message.py
@@ -1,5 +1,6 @@
 import class_notes as cn
 import navigation as nv
+import load_save_message as lm
 
 def read_note():
     indentifier=input("Найти ппо id, по заголовку либо по тексту \n")
@@ -14,3 +15,19 @@ def read_note():
             print()
             return
     return  print(f"Такой '{indentifier}' записи нет")
+
+def update_message():
+     indentifier=input("Найти ппо id, по заголовку либо по тексту \n")
+     for notes_elmiment in nv.message["notes"]:
+        if indentifier in str(notes_elmiment["id"] ) or indentifier in notes_elmiment["heading"]\
+            or indentifier in notes_elmiment["note_body"]:
+            heading = input(f"Редактируем заголовок ({notes_elmiment['heading']}): ")
+            note_body = input(f"Редактируем сообщение ({notes_elmiment['note_body']})  ")
+            notes_elmiment["heading"] = heading or notes_elmiment["heading"]
+            notes_elmiment["note_body"] = note_body or notes_elmiment["note_body"] 
+            notes_elmiment["last_modified_time"] = str(cn.datetime.now())
+            lm.save_message(nv.message)
+            print(f"Запись '{notes_elmiment['heading']}'обновлена")
+            return
+        print(f"Такой '{indentifier}' записи нет")
+            
