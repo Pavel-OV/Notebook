c567a84fcf222ed6e40e7c341b339adf57892722
Pavel <black_yaguar@mail.ru>
2023-06-13 08:59:18 +0500

    Визуальное оформление файла, убрал пустые строки и первые наброски кода

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import class_notes as cn
 import navigation as nav
-
-
-# timedate=(timedate.day, timedate.month, timedate.year)
-# print(f"Сегодня {timedate.day}-{timedate.month}-{timedate.year}")
-
-
-
 print( '''Привет! Это твоя записная книжка. Давай расскажу что я умею.
 Мы можем создать новую запись, найти её, изменить или удалить,
 а также показать все записи.''')
-
-
-
 if __name__ == '__main__':
    nav.main()
